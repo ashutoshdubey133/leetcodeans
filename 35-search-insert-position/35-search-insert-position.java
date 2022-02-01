3fd1bd8c0659080f2cf941054c37033a571f7500
Ashutosh Dubey <ashutoshdubey.cse@geu.ac.in>
2022-02-01 23:37:38 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (5.23%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        int mid;
+        while(start<end){
+            mid = (start+end)/2;
+            
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+        
+        return ((nums[start]>=target)?start:start+1);
+    }
+}

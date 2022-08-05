7126db1db90457bdb5069a5d9cd842b7decd71aa
Ashutosh Dubey <ashutoshdubey.cse@geu.ac.in>
2022-08-05 09:54:54 +0530

    Time: 504 ms (6.98%), Space: 62 MB (62.27%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.java
@@ -0,0 +1,37 @@
+class MyHashMap {
+    ArrayList<Integer> keys;
+    ArrayList<Integer> values;
+    
+    public MyHashMap() {
+         keys = new ArrayList<>();
+        values = new ArrayList<>();
+    }
+    
+    public void put(int key, int value) {
+        if(keys.contains(key)){
+            values.set(keys.indexOf(key),value);
+        }else{
+            keys.add(key);
+            values.add(value);
+        }
+    }
+    
+    public int get(int key) {
+        return keys.contains(key)?values.get(keys.indexOf(key)):-1;
+    }
+    
+    public void remove(int key) {
+        if(!keys.contains(key))
+            return;
+        values.remove(keys.indexOf(key));
+        keys.remove(keys.indexOf(key));
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
